9c03bc8b9c4d122991b18ce1750c8182ecc68fac
Dmitry Starov <dmitry.staroff@gmail.com>
2019-08-28 18:24:43 +0300

    Namespace instead of class

--- src/YNE.cpp
+++ src/YNE.cpp
@@ -1,55 +1,70 @@
 #include "YNE.hpp"
 #include "Arduino.h"
 
-YNE::YNE() {}
+namespace YNE {
+    namespace {
+        char _receivedData[30];
+        char _comp[32] = {0x01, 0x1C, 0x40, 0x00, 0x19, 0x3C, 0x44, 0x45, 0x56, 0x49, 0x43, 0x45, 0x3E, 0x52, 0x4F, 0x42,
+                        0x4F, 0x54, 0x3C, 0x2F, 0x44, 0x45, 0x56, 0x49, 0x43, 0x45, 0x3E, 0x0D, 0x0A, 0x0D, 0xC7, 0x1E};
 
-void YNE::begin() { Serial.begin(115200); }
+        const int _N = 7;
 
-void YNE::update() {
-    _receivedData[_N - 1] = Serial.read();
+        int _RM64 = 0;
+        int _RM65 = 0;
+        int _RM66 = 0;
+        int _RM67 = 0;
+    };
 
-    if (_receivedData[_N - 1] == -1) {
-        return;
+    void begin() {
+        Serial.begin(115200);
     }
 
-    if ((_receivedData[_N - 6] == '/') && (_receivedData[_N - 5] == 'r') && (_receivedData[_N - 4] == 'e') && (_receivedData[_N - 3] == 'g')) {
-        switch (_receivedData[_N - 2]) {
-            case 0x64:
-                _RM64 = _receivedData[_N - 1];
-                break;
-            case 0x65:
-                _RM65 = _receivedData[_N - 1];
-                break;
-            case 0x66:
-                _RM66 = _receivedData[_N - 1];
-                break;
-            case 0x67:
-                _RM67 = _receivedData[_N - 1];
-                break;
+    void update() {
+        _receivedData[_N - 1] = Serial.read();
+
+        if (_receivedData[_N - 1] == -1) {
+            return;
+        }
+
+        if ((_receivedData[_N - 6] == '/') && (_receivedData[_N - 5] == 'r') && (_receivedData[_N - 4] == 'e') && (_receivedData[_N - 3] == 'g')) {
+            switch (_receivedData[_N - 2]) {
+                case 0x64:
+                    _RM64 = _receivedData[_N - 1];
+                    break;
+                case 0x65:
+                    _RM65 = _receivedData[_N - 1];
+                    break;
+                case 0x66:
+                    _RM66 = _receivedData[_N - 1];
+                    break;
+                case 0x67:
+                    _RM67 = _receivedData[_N - 1];
+                    break;
+            }
+        }
+
+        if (strncmp(_receivedData, "/device", _N) == 0) {
+            Serial.write(_comp, 32);
+        }
+
+        for (size_t i = 1; i < _N; ++i) {
+            _receivedData[i - 1] = _receivedData[i];
         }
     }
 
-    if (strncmp(_receivedData, "/device", _N) == 0) {
-        Serial.write(_comp, 32);
+    int alpha() { return _RM64; }
+
+    int heart() { return _RM66; }
+
+    int muscle() {
+        int tmp = _RM67;
+
+        if (_RM67 < 0) {
+            tmp += 256;
+        }
+
+        return tmp;
     }
 
-    for (size_t i = 1; i < _N; ++i) {
-        _receivedData[i - 1] = _receivedData[i];
-    }
-}
-
-int YNE::alpha() { return _RM64; }
-
-int YNE::heart() { return _RM66; }
-
-int YNE::muscle() {
-    int tmp = _RM67;
-
-    if (_RM67 < 0) {
-        tmp += 256;
-    }
-
-    return tmp;
-}
-
-int YNE::p300() { return _RM65; }
+    int p300() { return _RM65; }
+};
